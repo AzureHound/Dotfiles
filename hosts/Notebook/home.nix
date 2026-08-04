360622dc9795b92786cacbcfb46df790c8c6c658
Eden <azurehound.landline090@slmail.me>
2026-08-04 13:42:41 +0530

    feat(host): Enabled kitty on Notebook

--- hosts/Notebook/home.nix
+++ hosts/Notebook/home.nix
@@ -28,6 +28,7 @@ in
     # chromium.enable = true;
     firefox.enable = true;
     foot.enable = true;
+    kitty.enable = true;
     qutebrowser.enable = true;
     sway.enable = true;
     thunderbird.enable = true;
