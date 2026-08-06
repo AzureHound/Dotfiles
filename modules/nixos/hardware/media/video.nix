fddda9a5cd9e2a56b1c8691de73a8cbd5b84b237
Eden <azurehound.landline090@slmail.me>
2026-08-07 00:25:55 +0530

    rm(nix): rm gpuless config

--- modules/nixos/hardware/media/video.nix
+++ modules/nixos/hardware/media/video.nix
@@ -1,1 +1,9 @@
-{ hardware.graphics.enable = true; }
+{ lib, config, ... }:
+
+let
+  inherit (lib.modules) mkDefault;
+in
+
+{
+  hardware.graphics.enable = mkDefault (config.pixel.profiles.graphical.enable || config.pixel.device.gpu != null);
+}
