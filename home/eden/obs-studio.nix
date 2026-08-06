b3bd86cf6374938a6f8465b586daddf592d0b802
Eden <azurehound.landline090@slmail.me>
2026-08-07 00:23:25 +0530

    rm(obs): rm dup deps

--- home/eden/obs-studio.nix
+++ home/eden/obs-studio.nix
@@ -4,15 +4,13 @@
   programs.obs-studio = {
     inherit (config.pixel.profiles.media.streaming) enable;
 
-    package = pkgs.obs-studio.override {
-      cudaSupport = true;
-    };
+    package = pkgs.pkgsCuda.obs-studio;
 
-    plugins = with pkgs.obs-studio-plugins; [
-      obs-move-transition
-      obs-multi-rtmp
-      obs-pipewire-audio-capture
+    plugins = with pkgs.pkgsCuda.obs-studio-plugins; [
       wlrobs
+      obs-multi-rtmp
+      obs-move-transition
+      obs-pipewire-audio-capture
     ];
   };
 
