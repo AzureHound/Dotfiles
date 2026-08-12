99e3b0fd2c80d444b5755db0808cc36a2f35121d
Eden <azurehound.landline090@slmail.me>
2026-08-12 21:25:56 +0530

    eden/swayimg: mv init.lua into module

--- home/eden/swayimg.nix
+++ home/eden/swayimg.nix
@@ -1,21 +1,47 @@
-{
-  lib,
-  pkgs,
-  config,
-  mkCfgLink,
-  ...
-}:
-
-let
-  inherit (lib.modules) mkIf;
-in
+{ pkgs, config, ... }:
 
 {
-  config = mkIf config.pixel.profiles.graphical.enable {
-    programs.swayimg = {
-      enable = config.pixel.profiles.graphical.enable && pkgs.stdenv.hostPlatform.isLinux;
-    };
+  programs.swayimg = {
+    enable = config.pixel.profiles.graphical.enable && pkgs.stdenv.hostPlatform.isLinux;
 
-    xdg.configFile = mkCfgLink [ "swayimg/init.lua" ];
+    initLua = ''
+      ---@diagnostic disable: undefined-global
+
+      swayimg.imagelist.adjacent = true
+
+      swayimg.viewer.set_window_background(0xff24273a)
+      swayimg.viewer.set_image_background(0xff24273a)
+      swayimg.viewer.loop = false
+
+      swayimg.slideshow.set_window_background(0xff24273a)
+      swayimg.slideshow.set_image_background(0xff24273a)
+
+      swayimg.gallery.thumb_size = 300
+      swayimg.gallery.window_color = 0xff24273a
+      swayimg.gallery.border_color = 0xffb7bdf8
+      swayimg.gallery.unselected_color = 0xff24273a
+
+      swayimg.text.visible = false
+
+      swayimg.viewer.on_key("q", function() swayimg.exit() end)
+      swayimg.gallery.on_key("q", function() swayimg.exit() end)
+
+      -- Viewer navigation
+      swayimg.viewer.on_key("Right", function() swayimg.viewer.open("next") end)
+      swayimg.viewer.on_key("Left",  function() swayimg.viewer.open("prev") end)
+      swayimg.viewer.on_key("Up",    function() swayimg.viewer.open("prev") end)
+      swayimg.viewer.on_key("Down",  function() swayimg.viewer.open("next") end)
+
+      swayimg.viewer.on_key("l", function() swayimg.viewer.open("next") end)
+      swayimg.viewer.on_key("h", function() swayimg.viewer.open("prev") end)
+      swayimg.viewer.on_key("k", function() swayimg.viewer.open("prev") end)
+      swayimg.viewer.on_key("j", function() swayimg.viewer.open("next") end)
+
+      -- Gallery navigation
+      swayimg.gallery.on_key("l", function() swayimg.gallery.select("right") end)
+      swayimg.gallery.on_key("h", function() swayimg.gallery.select("left") end)
+      swayimg.gallery.on_key("k", function() swayimg.gallery.select("up") end)
+      swayimg.gallery.on_key("j", function() swayimg.gallery.select("down") end)
+    '';
   };
 }
