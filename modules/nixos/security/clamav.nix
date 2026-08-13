7ccf2b4f7f6772681e45ecf9fb3b415d9b1ed74c
Eden <azurehound.landline090@slmail.me>
2026-08-13 16:23:34 +0530

    nixos/security/clamav: notify before 2 mins

--- modules/nixos/security/clamav.nix
+++ modules/nixos/security/clamav.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   services.clamav = {
     daemon.enable = true;
@@ -5,6 +7,20 @@
     scanner = {
       enable = true;
       interval = "*-*-10 00:00:00";
+    };
+  };
+
+  systemd.user = {
+    services.clamav-notify = {
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.libnotify}/bin/notify-send 'ClamAV' 'Scan starts in 2 minutes'";
+      };
+    };
+
+    timers.clamav-notify = {
+      wantedBy = [ "timers.target" ];
+      timerConfig.OnCalendar = "*-*-09 23:58:00";
     };
   };
 }
